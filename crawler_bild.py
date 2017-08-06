de05ad48a3b5b4e86d689d9e984db9f333c1f8f3
Andre <andre.basche@gmail.com>
2017-08-07 00:50:55 +0200

    Added posting for site 'bild'

---
+++ crawler_bild.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import re
+import logging
+import feedparser
+import html
+import requests
+import time
+import news
+import database
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+def main():
+    name = 'bild'
+    base_url = 'http://www.bild.de/'
+    channel_id = -1001142790377
+
+    check_site(name, base_url, channel_id)
+
+    data = get_data()
+    set_data(data, name)
+
+
+def get_data():
+    raw_data = feedparser.parse('http://www.bild.de/rssfeeds/vw-alles/vw-alles-26970192,sort=1,view=rss2.bild.xml')
+    data = []
+    for x in raw_data['entries']:
+        article = dict()
+        article['link'] = x['link']
+        text = x['summary']
+        text = re.split('\n', text, re.MULTILINE)
+        article['text'] = html.unescape(text[-1].split('<br />')[0])
+        title = html.unescape(x['title'])
+        article['title'] = title
+        img = x['media_thumbnail'][0]['url'].replace(',w=120,', ',w=1200,')
+        article['img'] = img
+        tags = x['tags']
+        tags = [y['term'] for y in tags]
+        article['tags'] = tags
+        data.append(article)
+    return data[::-1]
+
+
+def set_data(data, name):
+    for article in data:
+        n = news.News(name)
+        n.set_img(article['img'])
+        n.set_title(article['title'])
+        n.set_text(article['text'])
+        n.set_link(article['link'])
+        n.set_tags(article['tags'])
+        n.post()
+
+
+def check_site(name, link, channel_id):
+    db = database.Database()
+    if not db.check_site(name):
+        db.insert_site(name, link)
+        db.insert_channel(name, channel_id)
+
+
+if __name__ == '__main__':
+    main()
