dbbdc4d1b61a92cff4a842989d1c703a245361e3
Andre <andre.basche@gmail.com>
2019-02-22 13:29:54 +0100

    Change Telepolis to consistent view

--- examples/crawler_telepolis.py
+++ examples/crawler_telepolis.py
@@ -3,7 +3,10 @@
 
 
 import os
+import re
 import sys
+
+import requests
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 import logging
@@ -17,10 +20,20 @@ logging.basicConfig(
 )
 
 
+def get_img_and_tags(link):
+    source = requests.get(link).text
+    img = re.findall('property="og:image" *content="([^"]+)"', source)
+    tags = re.findall('name="keywords" content="([^"]+)"', source)
+    img = img[0] if img else ""
+    tags = tags[0].split(",") if tags else ""
+    return img, tags
+
+
 def main():
     site = Site()
     site.name = "telepolis_de"
     site.alias = "Telepolis.de"
+    site.short = "tp"
     site.base_url = "https://heise.de/tp"
     site.channel_id = -1001128692603
     site.instant_id = -1001342689984
@@ -30,8 +43,9 @@ def main():
     for x in raw_data["entries"]:
         if site.check_article_exists(x["link"]):
             continue
-        site.add_article(title=x["title"], link=x["link"], text=x["summary"])
-    site.post(1)
+        img, tags = get_img_and_tags(x["link"])
+        site.add_article(title=x["title"], link=x["link"], text=x["summary"], tags=tags, img=img)
+    site.post()
 
 
 if __name__ == "__main__":
